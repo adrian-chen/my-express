06b31e38092cdfefc37937eb239ce85e0f2fc7e8
Adrian Chen <adrianchen@Adrians-MacBook-Pro.local>
2015-02-15 12:05:12 -0700

    added heroku support

--- mongo.js
+++ mongo.js
@@ -21,7 +21,7 @@ require('./mongo/repoView')(app)
 require('./mongo/pullList')(app)
 require('./mongo/pullView')(app)
 
-var server = app.listen(3000, function() {
+var server = app.listen((process.env.PORT || 3000), function() {
 
     var host = server.address().address
     var port = server.address().port
